7bfed25b2f1fa3fea7d5b3d17c666ffde2c85fbd
Michael <pentium4.misha@gmail.com>
2013-05-23 16:22:32 +0300

    Check if files exist on page

--- Source/BusinessLayer/Configuration/InjectJs.cs
+++ Source/BusinessLayer/Configuration/InjectJs.cs
@@ -1,8 +1,11 @@
 ﻿
 namespace Sitecore.Feedback.Module.BusinessLayer.Configuration
 {
+  using Sitecore.Diagnostics;
   using Sitecore.Pipelines.RenderLayout;
   using Sitecore.Web;
+  using System;
+  using System.Collections.Specialized;
   using System.IO;
   using System.Web;
   using System.Web.UI;
@@ -15,19 +18,32 @@ namespace Sitecore.Feedback.Module.BusinessLayer.Configuration
       if (Context.Site.Name == "shell")
         return;
 
-      var head = WebUtil.FindControlOfType(Context.Page.Page, typeof(HtmlHead));
-      if (head != null)
+      try
       {
-        var filesJs = Directory.GetFiles(HttpContext.Current.Server.MapPath(Constants.FolderJs));
-        foreach (var fileJs in filesJs)
+        var head = WebUtil.FindControlOfType(Context.Page.Page, typeof(HtmlHead));
+        if (head != null)
         {
-          var extension = Path.GetExtension(fileJs);
-          if (extension != null && extension.Contains(".js"))
+          var filesJs = Directory.GetFiles(HttpContext.Current.Server.MapPath(Constants.FolderJs));
+          foreach (var fileJs in filesJs)
           {
-            IncludeJsToControl(head, Constants.FolderJs+Path.GetFileName(fileJs));
+            var extension = Path.GetExtension(fileJs);
+            if (extension != null && extension.Contains(".js"))
+            {
+              var fileName = Path.GetFileNameWithoutExtension(fileJs);
+              if (fileName != null && fileName.IndexOf('-') > 1)
+                fileName = fileName.Remove(fileName.IndexOf('-'));
+              if (!IsFileExistOnPage(head, fileName))
+              {
+                IncludeJsToControl(head, Constants.FolderJs + Path.GetFileName(fileJs));
+              }
+            }
           }
         }
       }
+      catch (Exception exception)
+      {
+        Log.Error("Feedback Module, InjectJs: ", exception,this);
+      }  
     }
 
     public Control IncludeJsToControl(Control control, string jsfile)
@@ -37,6 +53,43 @@ namespace Sitecore.Feedback.Module.BusinessLayer.Configuration
       child.Attributes.Add("src", jsfile);
       control.Controls.Add(child);
       return control;
+    }
+
+    private bool IsFileExistOnPage(Control head, string fileName)
+    {
+      var pathMainLayout = head.Page.TemplateControl.AppRelativeVirtualPath;
+      var fullPathMainLayouth = head.Page.MapPath(pathMainLayout);
+      var page = GetPage(fullPathMainLayouth, null);
+      return page.Contains(fileName);
+    }
+
+    public string GetPage(string url, NameValueCollection headers)
+    {
+      try
+      {
+        string ret = string.Empty;
+        System.Net.WebRequest myRequest = System.Net.WebRequest.Create(url);
+        myRequest.PreAuthenticate = true;
+        myRequest.Method = "GET";
+        if (headers != null)
+          myRequest.Headers.Add(headers);
+        System.Net.WebResponse myResponse = myRequest.GetResponse();
+        try
+        {
+          var stream = myResponse.GetResponseStream();
+          var streamreader = new System.IO.StreamReader(stream);
+          ret = streamreader.ReadToEnd();
+          return ret.Replace("\x00", "");
+        }
+        finally
+        {
+          myResponse.Close();
+        }
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Could not get HTML from " + url + ": " + ex.Message, ex);
+      }
     }
   }
 }
--- Source/PresentationLayer/FeedbackErrorPage.aspx
+++ Source/PresentationLayer/FeedbackErrorPage.aspx
@@ -8,7 +8,7 @@
     <title>Jetstream :: Error</title>
     <meta name="author">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <script type="text/javascript" src="/js/lib/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/css/jetstream.css">
 </head>
 <body class="base">
